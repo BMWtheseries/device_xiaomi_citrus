d70f97b0cc70dbd6b06f723d8f06cd547257f26c
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2023-05-14 18:53:46 +0700

    tes
    Change-Id: I9b2c433259967959735a40d83e0e908633200c8c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,11 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Kernel
+BOARD_PREBUILT_DTBOIMAGE := $(COMMON_PATH)-kernel/citrus/dtbo.img
+TARGET_PREBUILT_DTB := $(COMMON_PATH)-kernel/citrus/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_citrus
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_citrus
